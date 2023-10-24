3903d43a61ca35d2ba90874fde4c16df67b978d3
AndrejShylovich <andrejshylovich@gmail.com>
2023-10-24 17:39:35 +0300

    navbar + sign in/up screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,84 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
+
+import { useSelector } from 'react-redux'
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { useEffect } from 'react';
+
+
 
 function App() {
+  const { user } = useSelector((state) => state.auth)
+  const url = useLocation().pathname
+
+  useEffect(() => {
+    url && window.scrollTo(0, 0)
+  }, [url])
+
   return (
-    <div className="App">
-      <Navbar/>
-      App
+    <div>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Navbar />
+            App
+            <Footer />
+          </>
+        } />
+        <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/' />} />
+        <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
+        <Route path='/properties' element={
+          <>
+            <Navbar />
+            Properties
+            <Footer />
+          </>
+        } />
+        <Route path='/propertyDetail/:id' element={
+          <>
+            <Navbar />
+            propertyDetail
+            <Footer />
+          </>
+        } />
+        <Route path='/editproperty/:id' element={
+          user ?
+            <>
+              <Navbar />
+              editproperty
+              <Footer />
+            </>
+            : <Navigate to='/login' />
+        } />
+        <Route path='/my-profile' element={
+          user ?
+            <>
+              <Navbar />
+              my-profile
+              <Footer />
+            </>
+            : <Navigate to='/login' />
+        } />
+        <Route path='/update-profile' element={
+          user ?
+            <>
+              <Navbar />
+              update-profile
+              <Footer />
+            </>
+            : <Navigate to='/login' />
+        } />
+        <Route path='*' element={
+          <>
+            <Navbar />
+            *
+            <Footer />
+          </>
+        } />
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
 
 
